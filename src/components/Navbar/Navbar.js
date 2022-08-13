a7ab4eea0630249dc54c4775e653bdb934eee371
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-13 19:55:30 +0200

    Navbar links are  added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 const Navbar = () => {
   return (
-    <nav className="primary-navigation">
-      <ul>
-        <li>esraa</li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+    <nav className="Navbar__primary-navigation">
+      <ul role="list" className="Navbar__primary-list">
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Product</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Community</a></li>
       </ul>
     </nav>
   );
