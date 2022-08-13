6a127f7eca0b9a5921951c87cdf68e029e7f7d10
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-13 19:37:48 +0200

    header component is added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import images from "../../constants/images";
+import Navbar from "../Navbar/Navbar";
 const Header = () => {
   return (
     <header className="primary-header">
       <div className="container">
         <a href="#">
           <img src={images.logo} alt="logo" />
+          <Navbar/>
         </a>
       </div>
     </header>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="primary-navigation">
+      <ul>
+        <li>esraa</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
