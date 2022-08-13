fe8be697178fb536d76c860651f47670c3661e21
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-13 22:24:34 +0200

    SubHeading component is added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from "react";
+import SubHeading from "../SubHeading/SubHeading";
 const Main = () => {
-    return <div>main</div>;
-}
+  return (
+    <section>
+      <div className="Main__container">
+       <SubHeading/>
+      </div>
+    </section>
+  );
+};
 export default Main;
---
+++ src/components/SubHeading/SubHeading.js
@@ -0,0 +1,22 @@
+import React from "react";
+const SubHeading = () => {
+  return (
+    <div className="even-columns">
+      <div>
+        <h1 className="fs-primary-heading fw-bold">
+          Bring everyone together to build better products.
+        </h1>
+        <p>
+          Manage makes it simple for software teams to plan day-to-day tasks
+          while keeping the larger team goals in view.
+        </p>
+        <button className=""> Get Started</button>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+
+
+export default SubHeading;
