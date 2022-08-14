0d89e14d69cb9de3c728d685875d4ea136823277
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-14 19:20:56 +0200

    Create AboutTeam.js

---
+++ src/components/AboutTeam/AboutTeam.js
@@ -0,0 +1,23 @@
+import React from "react";
+const AboutTeam = () => {
+  return (
+    <section className="bg-primary-200 text-primary-100">
+      <div className="container">
+        <div className="even-columns">
+          <div>
+            {" "}
+            <p className="fs-secondary-heading fw-bold">
+              Simplify how your team works today.
+            </p>
+          </div>
+          <div>
+            <button className="button" datatype="inverted">
+ Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default AboutTeam;
