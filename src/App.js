cb8a99aed4e72ddba1f4fe694096df227ce811cd
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-14 19:20:21 +0200

    AboutTeam Component is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header/header";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
-      
+      <Footer/>
     </div>
   );
 }
--- src/components/carousal/Carousal.js
+++ src/components/carousal/Carousal.js
@@ -1,5 +1,10 @@
 import React from "react";
 const Carousal = () => {
-  return <section className="carousal"></section>;
+  return (
+    <section className="carousal">
+      <h2 className="fs-secondary-heading"> What they’ve said</h2>
+      <button className="Carousal__button"> Get Started</button>
+    </section>
+  );
 };
 export default Carousal;
