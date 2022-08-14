20cb4f834f1c94489b79cbee544dc952f17e2af6
Esraa22M <58557774+Esraa22M@users.noreply.github.com>
2022-08-14 19:21:07 +0200

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SubHeading from "../SubHeading/SubHeading";
 import About from "../About/About";
+import Carousal from "../carousal/Carousal";
+import AboutTeam from "../AboutTeam/AboutTeam";
 const Main = () => {
   return (
     <section>
       <div className="Main__container">
        <SubHeading/>
        <About/>
+       <Carousal/>
+       <AboutTeam/>
       </div>
     </section>
   );
